d6a381756a336cff72a61aa3cbd23eb4fd9aa658
Zinedine-Chekroun <165790750+Zinedine-Chekroun@users.noreply.github.com>
2024-04-12 12:10:36 +0200

    updt

--- __init__.py
+++ __init__.py
@@ -49,11 +49,11 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_nom/<int:post_nom>')
+@app.route('/fiche_nom/<string:post_nom>')
 def Readfiche2(post_nom):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_nom))
+    cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_nom))
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
