7b5f9db61a3cd9adc8cc8643792abc026d8f60af
Zinedine-Chekroun <165790750+Zinedine-Chekroun@users.noreply.github.com>
2024-04-12 14:18:40 +0200

    updt

--- __init__.py
+++ __init__.py
@@ -38,6 +38,20 @@ def authentification():
 
     return render_template('formulaire_authentification.html', error=False)
 
+@app.route('/authentification2', methods=['GET', 'POST'])
+def authentification2():
+    if request.method == 'POST':
+        # Vérifier les identifiants
+        if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
+            session['authentifie'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('lecture'))
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
+
+    return render_template('formulaire_authentification.html', error=False)
+
 
 
 @app.route('/fiche_client/<int:post_id>')
