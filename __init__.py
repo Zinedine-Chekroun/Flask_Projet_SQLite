577acb6066adba98fef2b910b12c957f138e05e8
Zinedine-Chekroun <165790750+Zinedine-Chekroun@users.noreply.github.com>
2024-04-12 12:51:44 +0200

    upd

--- __init__.py
+++ __init__.py
@@ -47,7 +47,7 @@ def authentification2():
         if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
             session['authentifie'] = True
             # Rediriger vers la route lecture après une authentification réussie
-            return redirect(url_for('/fiche_nom/<string:nom_client>'))
+            return redirect(url_for('lecture>'))
         else:
             # Afficher un message d'erreur si les identifiants sont incorrects
             return render_template('formulaire_authentification.html', error=True)
